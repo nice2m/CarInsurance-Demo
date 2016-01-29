7bce364d25cae44b1c195549404de430dfafd54d
千锋 <qianfeng@guanghuisuiyuemingziriqi-Mac-mini.local>
2016-01-29 09:40:28 +0800

    my

--- my-Project-Car/my-Project-Car/Classes/MVC/DetailPage/Controllers/DetailViewController.m
+++ my-Project-Car/my-Project-Car/Classes/MVC/DetailPage/Controllers/DetailViewController.m
@@ -9,7 +9,6 @@
 #import "DetailViewController.h"
 
 @interface DetailViewController ()
-@property (weak, nonatomic) IBOutlet UIToolbar *CARToolBar;
 
 @end
 
@@ -18,11 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    UIButton * btn = [UIButton buttonWithType:UIButtonTypeSystem];
-    btn.frame = CGRectMake(0, 0, 100, 40);
-    [btn setBackgroundColor:[UIColor redColor]];
-    UIBarButtonItem * firstItem = [[UIBarButtonItem alloc]initWithCustomView:btn];
-    self.toolbarItems = @[firstItem];
+    [self createUI];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -30,14 +26,77 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)createUI{
+    
+    //设置标题文本
+    UILabel * label = [[ UILabel alloc] initWithFrame:CGRectMake(0, 0, 60, 40)];
+    label.text = @"加载中…";
+    self.navigationItem.titleView = label;
+    
+    //设置左边按钮颜色
+    UINavigationItem * naviItem = self.navigationItem;
+    
+    UIButton * btnBack = [UIButton buttonWithType:UIButtonTypeCustom];
+    btnBack.frame = CGRectMake(0, 0, 30, 40);
+    [btnBack setImage:[UIImage imageNamed:@"return"] forState:UIControlStateNormal];
+    [btnBack addTarget:self action:@selector(backPressed:) forControlEvents:UIControlEventTouchUpInside];
+    naviItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:btnBack];
+    
+    
+    UIButton  * favBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    favBtn.frame = CGRectMake(0, 0, 40, 40);
+    [favBtn addTarget:self action:@selector(favPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [favBtn setImage:[UIImage imageNamed:@"fav_1"]forState:UIControlStateNormal];
+    [favBtn setImage:[UIImage imageNamed:@"fav_2"] forState:UIControlStateSelected];
+    naviItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:favBtn];
+    
+    
+    CGFloat btnW = kSCREEN_SIZE.width / 5 ;
+    UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(0, 0, btnW, 40);
+    
+    UIImage * imgNor = [UIImage imageNamed:@"recommend"];
+    UIImage * imgSel = [UIImage imageNamed:@"recommend_1"];
+    [btn setImage:imgNor forState:UIControlStateNormal];
+    [btn setImage:imgSel forState:UIControlStateSelected];
+    UIBarButtonItem * firstItem = [[UIBarButtonItem alloc]initWithCustomView:btn];
+    
+    UIButton * btn1 = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn1.frame = CGRectMake(0, 0, btnW, 40);
+    
+    UIImage *imgnor1 = [UIImage imageNamed:@"message"];
+    [btn1 setImage:imgnor1 forState:UIControlStateNormal];
+    UIBarButtonItem * secondItem = [[UIBarButtonItem alloc]initWithCustomView:btn1];
+    
+    UIButton * btn2 = [UIButton buttonWithType:UIButtonTypeCustom];
+    UIImage * imgNor2 = [UIImage imageNamed:@"share"];
+    [btn2 setImage: imgNor2 forState:UIControlStateNormal];
+    UIBarButtonItem * thirdItem = [[UIBarButtonItem alloc]initWithCustomView:btn2];
+    btn2.frame = CGRectMake(0, 0, btnW, 40);
+    //btn2.frame = btn.frame;
+    //self.toolbarItems = @[firstItem];
+    //显示ToolBar
+    self.navigationController.toolbarHidden = NO;
+    
+    //self.navigationController.toolbar.barTintColor = [UIColor orangeColor];
+    
+    //填充flexiable item 来布局 排版对齐方式
+    UIBarButtonItem * fix1 = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    UIBarButtonItem * fix2 = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    
+    [self setToolbarItems:@[fix1,firstItem,secondItem,thirdItem,fix2] animated:NO];
 }
-*/
+
+
+#pragma mark - selector
+
+-(void)backPressed:(UIButton *)sender{
+    NSLog(@"返回点击");
+}
+
+-(void)favPressed:(UIButton *)sender{
+    sender.selected = !sender.isSelected;
+    NSLog(@"右侧按钮点击");
+}
 
 @end
